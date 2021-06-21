e482b681f02321d5120fdfefb639d97c1156e4e4
Eniola <69263854+Ayodeji10@users.noreply.github.com>
2021-06-21 12:34:20 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import Home from './pages/home/Home'
 import Aspirant from './pages/aspirant/Aspirant'
 import About from './pages/about/About'
@@ -7,7 +7,9 @@ import Polls from './pages/polls/Polls'
 function App() {
   return (
     <Router>
-      <Route path='/' exact component={Home} />
+      <Route exact path="/" component={() => (<Redirect to='/home' />)} />
+      <Route exact path="/home" component={Home} />
+      {/* <Route path='/' exact component={Home} /> */}
       <Route path='/about' component={About} />
       <Route path='/aspirant' component={Aspirant} />
       <Route path='/polls' component={Polls} />
